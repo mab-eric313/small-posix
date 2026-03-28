4a1c0ba563baf82236f251ebd292848495c05482
mab-eric313 <kseneric313@gmail.com>
2026-03-28 09:20:13 +0700

    Refactor mkdir argument handling
    Improves readability by using for-loop and removing redudant logic

--- src/mkdir.c
+++ src/mkdir.c
@@ -6,19 +6,17 @@
 int main(int argc, char **argv) {
 	char *pathname[PATHNAME_SIZE];
 
-	int i = 0;
-	if (argc > 1)
-		while (++i < argc) {
-			printf("argv[i]: %s\n", argv[i]);
-			pathname[i] = argv[i];
-			if (mkdir(pathname[i], 0777) == -1) {
-				perror("error: mkdir failed to create a dir\n");
-				return -1;
-			}
-		}
-	else {
-		perror("Usage: mkdir [pathname]\n");
+	if (argc <= 1) {
+		fprintf(stderr, "usage: mkdir [DIRECTORY...]\n");
 		return -1;
+	}
+
+	for (int i = 1; i < argc; i++) {
+		pathname[i] = argv[i];
+		if (mkdir(pathname[i], 0777) == -1) {
+			perror("error: mkdir failed to create a dir");
+			return -1;
+		}
 	}
 
 	return 0;
