9d45ea98b42f501377bc55bbe524c0802c69f8a6
mab-eric313 <kseneric313@gmail.com>
2026-04-04 16:26:01 +0700

    refactor: improve robustness and directory handling in cp
    - Replace manual exists check with stat() logic
    - Fix memory corruption by changing buffer to char array
    - Use snprintf and basename for safer path construction
    - Implement proper file descriptor cleanup to prevent leaks
    - Use PATH_MAX from linux/limits.h for buffer safety

--- src/cp.c
+++ src/cp.c
@@ -4,79 +4,64 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <libgen.h>
+#include <linux/limits.h>
 
 #define BUFFER_SIZE 4096
 
-int is_regular_file(const char *path) {
-    struct stat path_stat;
-    if (stat(path, &path_stat) < 0) {
-		return -1;
-	}
-    return S_ISREG(path_stat.st_mode);
-}
-
-int is_exist(const char *path) {
-	if (is_regular_file(path) == 1) { // 'path' is exist and is a file
-		return 1;
-	} else if (is_regular_file(path) == 0) { // 'path' is directory
-		return 0;
-	} else { // 'path' doesn't exist
-		return -1;
-	}
-}
-
 int main(int argc, char **argv) {
-	char *buffer[BUFFER_SIZE];
-	int src_fd, dst_fd, dst_dirfd;
-	src_fd = dst_fd = dst_dirfd = -1;
-
 	if (argc <= 2) {
-		fprintf(stderr, "Usage: cp [SOURCES...] [DEST FILES...]\n");
+		fprintf(stderr, "Usage: %s [SOURCES...] [DEST FILES...]\n", argv[0]);
 		return -1;
 	}
+
+	struct stat st_dest;
+	int dest_is_dir = (stat(argv[argc-1], &st_dest) == 0 && 
+						S_ISDIR(st_dest.st_mode));
 
 	for (int i = 1; i < argc-1; i++) {
-		struct stat st_file;
-		int src_st_mode;
+		struct stat st_src;
+		char path_buffer[PATH_MAX];
+		char *dest_path;
 
-		if (stat(argv[i], &st_file) < 0) {
-			perror("stat() error");
-			return -1;
+		if (stat(argv[i], &st_src) < 0) {
+			perror("stat() source file error");
+			continue;
 		}
 
-		src_st_mode = st_file.st_mode;
+		if (dest_is_dir) {
+			char *base = basename(argv[i]);
+			snprintf(path_buffer, sizeof(path_buffer), "%s/%s", argv[argc-1], base);
+			dest_path = path_buffer;
+		} else {
+			dest_path = argv[argc-1];
+		}
 
-		if (is_exist(argv[argc-1]) == 0) {
-			dst_dirfd = open(argv[argc-1], O_RDONLY | O_DIRECTORY);
-
+		int src_fd = open(argv[i], O_RDONLY);
 		// TODO: add confirmation, if there is a file in 'path', can i overwrite it?
-			dst_fd = openat(dst_dirfd, argv[i], O_TRUNC | O_WRONLY | O_CREAT, src_st_mode);
-		} else if (is_exist(argv[argc-1]) == -1) {
-			dst_fd = open(argv[argc-1], O_WRONLY | O_CREAT, src_st_mode);
+		int dst_fd = open(dest_path, O_WRONLY | O_CREAT | O_TRUNC, st_src.st_mode);
+
+		if (src_fd < 0 || dst_fd < 0) {
+			perror("open() error");
+			if (src_fd >= 0) close(src_fd);
+			if (dst_fd >= 0) close(dst_fd);
+			continue;
 		}
 
-		if (dst_fd < 0) {
-			perror("open() destination file error");
-			return -1;
-		}
-
-		src_fd = open(argv[i], O_RDONLY);
-		if (src_fd < 0) {
-			perror("open() source file error");
-			return -1;
-		}
-
-		int ret;
-		while ((ret = read(src_fd, buffer, BUFFER_SIZE)) > 0) {
-			buffer[ret] = 0x00;
-			if (write(dst_fd, buffer, ret) != ret) {
+		char buffer[BUFFER_SIZE];
+		ssize_t n_read;
+		while ((n_read = read(src_fd, buffer, BUFFER_SIZE)) > 0) {
+			if (write(dst_fd, buffer, n_read) != n_read) {
 				perror("write() error");
-				return -1;
+				break;
 			}
 		}
 
+		if (n_read < 0) {
+			perror("read() read source file error");
+		}
+
 		close(src_fd);
-		close(dst_dirfd);
 		close(dst_fd);
 	}
 
