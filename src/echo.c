91cbca83412706f616b9e77c17d85534c12ad289
mab-eric313 <kseneric313@gmail.com>
2026-04-18 14:31:52 +0700

    add '-n' option

--- src/echo.c
+++ src/echo.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
-void echo(char **s) {
+void echo(char *s) {
 	static int first = 1;
 	if (!first) {
 		fputc(' ', stdout);
 	}
-	fputs(*s, stdout);
+	fputs(s, stdout);
 	first = 0;
 }
 
 int main(int argc, char **argv) {
-	while (--argc >= 1)
-		echo(++argv);
+	int nflag = 0;
 
-	putchar('\n');
+	if (strcmp(argv[1], "-n") == 0) {
+		nflag = 1;
+		argc -= 2;
+		argv += 2;
+	} else {
+		argc--, argv++;
+	}
+
+	for (; *argv; argc--, argv++)
+		echo(*argv);
+
+	if (nflag == 0)
+		putchar('\n');
 
 	return 0;
 }
